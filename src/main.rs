653ed45e17279b791f44fd077b87f2d5e0addb45
Jakub Jarosz <jakub@jarosz.dev>
2025-02-28 16:39:10 +0000

    Add all band colours

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::io;
 
 fn main() {
     println!("Please enter band colour:");
+    println!(
+        "Available colours: black, brown, red, orange, yellow, green, blue, violet, grey, white"
+    );
 
     let band1 = get_band_input();
     let band2 = get_band_input();
     let band3 = get_band_input();
 
     let r1 = ((band1 * 10) + band2) * 10_u32.pow(band3);
-    println!("{r1}");
+    println!("{r1} Ohms");
 }
 
 fn get_band_input() -> u32 {
@@ -19,9 +22,16 @@ fn get_band_input() -> u32 {
         .expect("Failed to read the line");
 
     let val = match color.trim() {
-        "red" => 2,
-        "brown" => 1,
         "black" => 0,
+        "brown" => 1,
+        "red" => 2,
+        "orange" => 3,
+        "yellow" => 4,
+        "green" => 5,
+        "blue" => 6,
+        "violet" => 7,
+        "grey" => 8,
+        "white" => 9,
         _ => unreachable!("PANIC!"),
     };
     val
